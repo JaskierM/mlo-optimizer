47eaf8cf2baad5c83fdea00c84df37aa4b0a1dde
Borodynya Maxim <borody.maxim@gmail.com>
2023-11-05 21:23:40 +0300

    Добавлена ссылка на репозиторий в GitHub

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../mlo_optimizer'))
 project = 'Matrix Optimization of Object Locations'
 copyright = '2023, Jaskier'
 author = 'Jaskier'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
